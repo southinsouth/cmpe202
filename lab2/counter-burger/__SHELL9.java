26ce5223ac42ceb045860c6c626cf867dc3f13e5
Xiaoting Jin <xajin@linkedin.com>
2019-02-19 23:26:41 -0800

    lab#2 first commit

---
+++ lab2/counter-burger/__SHELL9.java
@@ -0,0 +1,11 @@
+
+
+public class __SHELL9 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/xajin/Courses/Spring_2019/CMPE-202-Ent-SW-Eng/assignments/cmpe202/lab2/counter-burger");
+final Client client1 = (Client)__bluej_runtime_scope.get("client1");
+
+
+client1.runTest();
+
+}}
